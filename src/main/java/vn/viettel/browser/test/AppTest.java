8c64134d4d9a177edf25b7132d5d261a8da34fc0
cuongdm29 <dmcksclck55@gmail.com>
2017-08-15 15:37:45 +0700

    Add code thread

--- src/main/java/vn/viettel/browser/test/AppTest.java
+++ src/main/java/vn/viettel/browser/test/AppTest.java
@@ -6,23 +6,30 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import vn.viettel.browser.config.AppConfig;
 import vn.viettel.browser.worker.SendMessWorker;
+import vn.viettel.browser.worker.SetListFirsebaseIdToQueue;
 
 public class AppTest {
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 	    ThreadPoolTaskExecutor taskExecutor = (ThreadPoolTaskExecutor) context.getBean("taskExecutor");
+        
+	    SetListFirsebaseIdToQueue setListFirsebaseIdToQueue = (SetListFirsebaseIdToQueue) context.getBean("setListFirsebaseIdToQueue");
+	    taskExecutor.execute(setListFirsebaseIdToQueue);
+	    
+	    SendMessWorker sendMessWorker1 = (SendMessWorker) context.getBean("sendMessWorker");
+	    sendMessWorker1.setName("sendMessWorker1");
+	    taskExecutor.execute(sendMessWorker1);
+	    
+	    
+	    SendMessWorker sendMessWorker2 = (SendMessWorker) context.getBean("sendMessWorker");
+	    sendMessWorker2.setName("sendMessWorker2");
+	    taskExecutor.execute(sendMessWorker2);
+	    
+	    SendMessWorker sendMessWorker3 = (SendMessWorker) context.getBean("sendMessWorker");
+	    sendMessWorker3.setName("sendMessWorker3");
+	    taskExecutor.execute(sendMessWorker3);
 
-	    SendMessWorker printTask1 = (SendMessWorker) context.getBean("sendMessWorker");
-	    taskExecutor.execute(printTask1);
-
-	   /* PrintTask2 printTask2 = (PrintTask2) context.getBean("printTask2");
-	    printTask2.setName("Thread 2");
-	    taskExecutor.execute(printTask2);
-
-	    PrintTask2 printTask3 = (PrintTask2) context.getBean("printTask2");
-	    printTask3.setName("Thread 3");
-	    taskExecutor.execute(printTask3);
-
+	   
 		for (;;) {
 			int count = taskExecutor.getActiveCount();
 			System.out.println("Active Threads : " + count);
@@ -35,7 +42,7 @@ public class AppTest {
 				taskExecutor.shutdown();
 				break;
 			}
-		}*/
+		}
 
 	}
 
--- src/main/java/vn/viettel/browser/worker/SendMessWorker.java
+++ src/main/java/vn/viettel/browser/worker/SendMessWorker.java
@@ -12,23 +12,31 @@ import vn.viettel.browser.ultils.FireBaseUtils;
 @Scope("prototype")
 public class SendMessWorker implements Runnable {
 
-	/*String message;
-
-	public void setMessage(String message) {
+	String message;
+    float time1;
+    float time2;
+	public void setName(String message) {
+		time1 = System.currentTimeMillis();
 		this.message = message;
-	}*/
+	}
 	@Override
 	public void run() {
 		try {
-			JSONObject results = new JSONObject();
-			JSONObject data = new JSONObject();
-			data.put("articleId", "sssssssssssssssss");
-			data.put("title", "sssssssssssssssss");
-			data.put("image", "sssssssssssssssss");
-			results.put("data", data);
-			results.put("to", SetListFirsebaseIdToQueue.namesQueue.peek());
-			FireBaseUtils.pushNotificationToSingleDevice(results);
-			System.out.println("@results_android : " + results);
+			while(SetListFirsebaseIdToQueue.namesQueue.size() > 0) {
+				JSONObject results = new JSONObject();
+				JSONObject data = new JSONObject();
+				data.put("articleId", "sssssssssssssssss");
+				data.put("title", "sssssssssssssssss");
+				data.put("image", "sssssssssssssssss");
+				results.put("data", data);
+				results.put("to", SetListFirsebaseIdToQueue.namesQueue.poll());
+				
+				System.out.println("Size Queue SendMessWorker....... " +  message + "......... "+ SetListFirsebaseIdToQueue.namesQueue.size());
+				
+				FireBaseUtils.pushNotificationToSingleDevice(results);
+				System.out.println("@results_android : " + results);
+			}
+			time2 = System.currentTimeMillis();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -36,6 +44,8 @@ public class SendMessWorker implements Runnable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("time of " + message + ".... " + (time2 -time1)/1000);
+		
 
 	}
 
